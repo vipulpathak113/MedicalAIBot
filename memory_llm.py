ac1fff7e9cd77593356b28600fa0cc2177e9bfae
Yoda_Vipul <vipulpathak113@gmail.com>
2025-03-27 01:08:29 +0530

    create vector embeddings

--- memory_llm.py
+++ memory_llm.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 # Load all PDF files in the data directory
 loader = PyPDFDirectoryLoader(
@@ -12,3 +14,7 @@ documents = loader.load()
 # Split the text into characters
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 splitted_chunks = splitter.split_documents(documents)
+
+# Create Vector Embeddings
+embeddings_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
