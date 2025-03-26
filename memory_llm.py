17dea8d7a05f4772f295238d5ee1432d12c5ca19
Yoda_Vipul <vipulpathak113@gmail.com>
2025-03-27 01:00:25 +0530

    Load documents from directory

--- memory_llm.py
+++ memory_llm.py
@@ -1,6 +1,10 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+# Load all PDF files in the data directory
 loader = PyPDFDirectoryLoader(
-    path = "./example_data/",
-    glob = "**/[!.]*.pdf",
+    path="data/",
+    glob="**/[!.]*.pdf",
 )
+
+documents = loader.load()
